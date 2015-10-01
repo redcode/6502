0fbf15b3864caef6f955833a14981d3ccaa7e52d
redcode <manuel@redcodesoftware.com>
2015-10-01 20:50:30 +0200

    Small update.

--- 6502.CPU/v1.0/API/C/emulation/CPU/6502.h
+++ 6502.CPU/v1.0/API/C/emulation/CPU/6502.h
@@ -1,4 +1,4 @@
-/* MOS 6502 CPU Emulator v1.0
+/* MOS 6502 CPU Emulator
   ____    ____    ___ ___     ___
  / __ \  / ___\  / __` __`\  / __`\
 /\ \/  \/\ \__/_/\ \/\ \/\ \/\  __/
@@ -11,10 +11,18 @@ Released under the terms of the GNU General Public License v3. */
 #define __emulation_CPU_6502_H__
 
 #include <Z/hardware/CPU/architecture/650x.h>
-#include <Z/types/generic functions.h>
+#include <Z/ABIs/emulation.h>
 
-#ifndef CPU_6502_NO_SLOTS
+#ifdef CPU_6502_USE_SLOTS
 #	include <Z/macros/slot.h>
+#endif
+
+#ifndef CPU_6502_API
+#	if defined(CPU_6502_USE_STATIC)
+#		define CPU_6502_API
+#	else
+#		define CPU_6502_API Z_API
+#	endif
 #endif
 
 typedef struct {
@@ -25,48 +33,36 @@ typedef struct {
 	zboolean   nmi;
 	zboolean   irq;
 
-#	ifdef CPU_6502_NO_SLOTS
+#	ifdef CPU_6502_USE_SLOTS
+		struct {ZSlot(Z16BitAddressRead8Bit ) read;
+			ZSlot(Z16BitAddressWrite8Bit) write;
+		} cb;
+#	else
 		void* cb_context;
 
 		struct {Z16BitAddressRead8Bit  read;
 			Z16BitAddressWrite8Bit write;
 		} cb;
-#	else
-		struct {ZSlot(Z16BitAddressRead8Bit ) read;
-			ZSlot(Z16BitAddressWrite8Bit) write;
-		} cb;
 #	endif
 } M6502;
 
-#if !defined(CPU_6502_BUILDING_DYNAMIC) && !defined(CPU_6502_BUILDING_STATIC)
+Z_C_SYMBOLS_BEGIN
 
-#	if defined(CPU_6052_USE_STATIC)
-#		define CPU_6502_API
-#	else
-#		define CPU_6502_API Z_API
-#	endif
+CPU_6502_API extern ZCPUEmulatorABI const abi_cpu_6502;
 
-#	include <Z/ABIs/emulation.h>
+CPU_6502_API zsize m6502_run   (M6502*	 object,
+				zsize	 cycles);
 
-	Z_C_SYMBOLS_BEGIN
+CPU_6502_API void  m6502_power (M6502*	 object,
+				zboolean state);
 
-	CPU_6502_API extern ZCPUEmulatorABI const abi_cpu_6502;
+CPU_6502_API void  m6502_reset (M6502*	 object);
 
-	CPU_6502_API zsize m6502_run   (M6502*	 object,
-					zsize	 cycles);
+CPU_6502_API void  m6502_nmi   (M6502*	 object);
 
-	CPU_6502_API void  m6502_power (M6502*	 object,
-					zboolean state);
+CPU_6502_API void  m6502_irq   (M6502*	 object,
+				zboolean state);
 
-	CPU_6502_API void  m6502_reset (M6502*	 object);
-
-	CPU_6502_API void  m6502_nmi   (M6502*	 object);
-
-	CPU_6502_API void  m6502_irq   (M6502*	 object,
-					zboolean state);
-
-	Z_C_SYMBOLS_END
-
-#endif
+Z_C_SYMBOLS_END
 
 #endif /* __emulation_CPU_6502_H__ */
--- 6502.CPU/v1.0/sources/6502.c
+++ 6502.CPU/v1.0/sources/6502.c
@@ -1,4 +1,4 @@
-/* MOS 6502 CPU Emulator v1.0
+/* MOS 6502 CPU Emulator
   ____    ____    ___ ___     ___
  / __ \  / ___\  / __` __`\  / __`\
 /\ \/  \/\ \__/_/\ \/\ \/\ \/\  __/
@@ -7,12 +7,16 @@
 Copyright © 1999-2015 Manuel Sainz de Baranda y Goñi.
 Released under the terms of the GNU General Public License v3. */
 
-#include <emulation/CPU/6502.h>
-
 #if defined(CPU_6502_BUILDING_DYNAMIC)
-#	define API Z_API_EXPORT
+#	define CPU_6502_API Z_API_EXPORT
 #else
-#	define API
+#	define CPU_6502_API
+#endif
+
+#ifdef CPU_6502_USE_LOCAL_HEADER
+#	include "6502.h"
+#else
+#	include <emulation/CPU/6502.h>
 #endif
 
 
@@ -39,12 +43,12 @@ typedef zuint8 (* Instruction)(M6502 *object);
 
 /* MARK: - Macros & Functions: Callback */
 
-#ifdef CPU_6502_NO_SLOTS
-#	define CB_ACTION(name) object->cb.name
-#	define CB_OBJECT(name) object->cb_context
-#else
+#ifdef CPU_6502_USE_SLOTS
 #	define CB_ACTION(name) object->cb.name.action
 #	define CB_OBJECT(name) object->cb.name.object
+#else
+#	define CB_ACTION(name) object->cb.name
+#	define CB_OBJECT(name) object->cb_context
 #endif
 
 #define READ_8(address)		CB_ACTION(read )(CB_OBJECT(read ), (address))
@@ -793,7 +797,7 @@ static Instruction const instruction_table[256] = {
 
 /* MARK: - Main Functions */
 
-API zsize m6502_run(M6502 *object, zsize cycles)
+CPU_6502_API zsize m6502_run(M6502 *object, zsize cycles)
 	{
 	/*------------.
 	| Clear ticks |
@@ -849,7 +853,7 @@ API zsize m6502_run(M6502 *object, zsize cycles)
 	}
 
 
-API void m6502_reset(M6502 *object)
+CPU_6502_API void m6502_reset(M6502 *object)
 	{
 	PC = 0; //READ_POINTER(RESET);
 	S = 0xFF;
@@ -862,9 +866,9 @@ API void m6502_reset(M6502 *object)
 	}
 
 
-API void m6502_power(M6502 *object, zboolean state) {if (state) m6502_reset(object);}
-API void m6502_nmi  (M6502 *object)		    {NMI = TRUE ;}
-API void m6502_irq  (M6502 *object, zboolean state) {IRQ = state;}
+CPU_6502_API void m6502_power(M6502 *object, zboolean state) {if (state) m6502_reset(object);}
+CPU_6502_API void m6502_nmi  (M6502 *object)		     {NMI = TRUE ;}
+CPU_6502_API void m6502_irq  (M6502 *object, zboolean state) {IRQ = state;}
 
 
 #ifdef CPU_6502_BUILDING_MODULE
@@ -886,7 +890,7 @@ API void m6502_irq  (M6502 *object, zboolean state) {IRQ = state;}
 		{Z_EMULATOR_OBJECT_MEMORY, Z_EMULATOR_ACTION_WRITE_8BIT, SLOT_OFFSET(write)}
 	};
 
-	API ZCPUEmulatorABI const abi_cpu_6502 = {
+	CPU_6502_API ZCPUEmulatorABI const abi_cpu_6502 = {
 		0, NULL, 5, exports, {sizeof(M6502), Z_OFFSET_OF(M6502, state), 2, slot_linkages}
 	};
 
