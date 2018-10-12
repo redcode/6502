d11386071b5f90d6022da9bdba2b6d6e5d4f22d8
redcode <manuel@zxe.io>
2018-10-12 03:40:53 +0200

    premake4.lua

--- building/premake4.lua
+++ building/premake4.lua
@@ -9,7 +9,7 @@ solution "6502"
 		flags {"ExtraWarnings"}
 		files {"../sources/**.c"}
 		includedirs {"../API"}
-		--buildoptions {"-std=c89 -pedantic -Wall"}
+		--buildoptions {"-std=c89 -pedantic -Wall -Weverything"}
 
 		configuration "Release*"
 			targetdir "lib/release"
